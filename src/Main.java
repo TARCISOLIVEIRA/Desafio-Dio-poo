a0aa86a578e6425139c40c55490083d0008de48c
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2022-10-20 22:52:17 -0300

    Atualizando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("curso Java");
+        curso.setDescricao("descriao");
+        curso.setCargahoraria(8);
+
+
+        System.out.print(curso);
+
     }
+
+
+
 }
